f11bb38ccd59162daa9f0483658de505e34f793d
Валера Миронов <valik10205@gmail.com>
2020-03-28 19:25:12 +0300

    Добавил .gitignore и удалил ненужные папки

--- students/y2335/practical_works/Anisimov/django_project_Anisimov/project_first_app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import(Owner)
-from .models import(Ownership)
-from .models import(Car)
-
-admin.site.register(Owner)
-admin.site.register(Ownership)
-admin.site.register(Car)
-# Register your models here.
