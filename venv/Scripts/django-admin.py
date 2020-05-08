0c4eb2b3a863942dd1f5a22165dde04360a472c5
Валера Миронов <valik10205@gmail.com>
2020-05-08 19:41:21 +0300

    Вставил гитигнор адекватно и присобачил его через командную строку (или какой тут глагол стоит употребить корректнее?), теперь все точно работает и все в полном ажуре!

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\Valera\Desktop\ITMO_FSPO_Web_Django_2020-master\students\y2333\practical_works\Valera_MIronov\Praktos_1\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
