adf55eb89d6fcfea1dad60feda4597449b48111a
blackmoledead <>
2020-07-01 23:24:16 +0300

    Bereza Vladimir y2333 4-th and finally

---
+++ students/y2333/practical_works/bereza_vladmir/final/app/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import *
+
+
+admin.site.register(Employer)
+admin.site.register(Vacancy)
+admin.site.register(Job_seeker)
+admin.site.register(Summary)
+admin.site.register(Response)
+# Register your models here.
