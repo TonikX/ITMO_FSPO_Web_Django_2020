87833d889e360944c2a7638906f07d30ef3194b3
mrSalamanders <42103998+mrSalamanders@users.noreply.github.com>
2020-07-02 00:36:21 +0300

    Summary
    очистка

--- students/y2331/laboratory_works/Nikolaev_Vladislav/laboratory_work_1/lab_work_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class LabWorkAppConfig(AppConfig):
-    name = 'lab_work_app'
--- students/y2331/practical_works/Nikolaev_Vladislav/simple_django_web_project/lesson2_11022020/venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/mrsalamanders/Documents/GitHub/ITMO_FSPO_Web_Django_2020-master/students/y2331/practical_works/Nikolaev_Vladislav/simple_django_web_project/lesson2_11022020/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
