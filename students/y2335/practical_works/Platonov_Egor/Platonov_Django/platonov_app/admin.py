cc1ffe48ea1d76a1257fef6a22cfcd8185d90dbf
Egor <gyvacha@gmail.com>
2020-07-04 20:52:58 +0300

    Практическая работа 3. Платонов Егор Y2335

--- students/y2335/practical_works/Platonov_Egor/Platonov_Django/platonov_app/admin.py
+++ students/y2335/practical_works/Platonov_Egor/Platonov_Django/platonov_app/admin.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
 
-from .models import Auto
-from .models import Owner
-from .models import Owning
-from .models import DriveLic
+from .models import *
 
 admin.site.register(Auto)
 admin.site.register(Owner)
 admin.site.register(Owning)
 admin.site.register(DriveLic)
+admin.site.register(userOwner)
