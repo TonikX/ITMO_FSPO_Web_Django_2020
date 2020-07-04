d063a64da825f5db8c3500ed189e21a216abadb7
Egor <gyvacha@gmail.com>
2020-07-04 16:33:37 +0300

    Исправлен gitignore, изменена папка практической работы

--- students/y2335/practical_works/Platonov_Egor/Pr1/venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\Egor\PycharmProjects\lesson2_11022020\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
