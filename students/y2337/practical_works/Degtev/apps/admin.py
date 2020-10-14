9d59d6630c72f9a27bce0f7a98b6cde6fe4b3403
Sagandh <sagaty236@gmail.com>
2020-10-14 03:34:14 +0300

    1-3

---
+++ students/y2337/practical_works/Degtev/apps/admin.py
@@ -0,0 +1,13 @@
+# Register your models here.
+from django.contrib import admin
+
+from .models import *
+
+admin.site.register(Owner)
+admin.site.register(Auto)
+admin.site.register(Certification)
+admin.site.register(Possession)
+admin.site.register(User)
+
+
+
